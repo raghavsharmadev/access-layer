f6104844f25e95b04382d0698767e0a0fa643c96
raghavsharmadev <dev.raghavsharma@gmail.com>
2026-03-23 16:12:47 +0000

    first commit

---
+++ src/main/java/com/rs/accesslayer/AccessLayerApplication.java
@@ -0,0 +1,13 @@
+package com.rs.accesslayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessLayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessLayerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rs/accesslayer/AccessLayerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rs.accesslayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessLayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
