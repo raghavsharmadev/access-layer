ccfa84f3cb9e09864c5a36164f640a77efc07d43
raghavsharmadev <dev.raghavsharma@gmail.com>
2026-04-04 17:38:59 +0000

    configs added using env

--- src/main/java/com/rs/accesslayer/utitlity/JwtUtility.java
+++ src/main/java/com/rs/accesslayer/utitlity/JwtUtility.java
@@ -7,12 +7,18 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import java.util.Date;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class JwtUtility {
 
-    private static final String SECRET_KEY = "a8f9c2d4e7b1a6c9d3f8e2a1b5c7d9f0a4e6b8c1d2f3a5b7c9d1e2f4a6b8c0d2"; // Use a secure key from config
+    private static String SECRET_KEY; // Use a secure key from config
+
+    @Value("${jwt.secret}")
+    private void setSecretKey(String secretKey) {
+        SECRET_KEY = secretKey;
+    }
 
     public static String generateToken(final User user) {
         return Jwts.builder()
